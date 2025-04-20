e01c013693ad787c5a872bd95c6045a9a0d1e88b
Leonel Menci <minterger@gmail.com>
2025-04-19 22:48:38 -0300

    feat: add @tailwindcss/postcss plugin and update postcss configuration
    - Added @tailwindcss/postcss as a dependency in package.json.
    - Updated pnpm-lock.yaml to include new dependencies and their resolutions.
    - Modified postcss.config.js to import and use the new @tailwindcss/postcss plugin alongside tailwindcss and autoprefixer.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// postcss.config.js
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import tailwindcss_postcss from "@tailwindcss/postcss"; // Importa el nuevo plugin
+
+export default {
+  plugins: [
+    tailwindcss,
+    tailwindcss_postcss(), // Usa la función para el plugin de Tailwind CSS
+    autoprefixer,
+  ],
+};
